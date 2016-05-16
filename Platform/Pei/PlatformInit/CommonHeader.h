4c3e6ec39eede16608000c3f8c855efeb3a3c6f9
Lee Leahy <leroy.p.leahy@intel.com>
2016-05-16 07:39:32 -0700

    QuarkFsp: Remove I2C chip initialization
    Remove the I2C chip initialization from FSP.  This platform specific
    initialization needs to be done in the BIOS code!
    
    Change-Id: I7dde2c9ac0fd831a362f3f459670273370be0425
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Lee Leahy <leroy.p.leahy@intel.com>

--- Platform/Pei/PlatformInit/CommonHeader.h
+++ Platform/Pei/PlatformInit/CommonHeader.h
@@ -23,32 +23,23 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #include <IntelQNCPeim.h>
 #include "Ioh.h"
 #include <Platform.h>
-#include <PlatformBoards.h>
 
 #include <IndustryStandard/SmBus.h>
 #include <IndustryStandard/Pci22.h>
 
 #include <Guid/AcpiS3Context.h>
 #include <Ppi/AtaController.h>
-//#include <Guid/Capsule.h>
 #include <Ppi/MasterBootMode.h>
 #include <Guid/MemoryTypeInformation.h>
-//#include <Guid/RecoveryDevice.h>
 #include <Guid/MemoryConfigData.h>
 #include <Guid/MemoryOverwriteControl.h>
-//#include <Guid/CapsuleVendor.h>
-//#include <Guid/QuarkCapsuleGuid.h>
 #include <Ppi/ReadOnlyVariable2.h>
 #include <Ppi/FvLoadFile.h>
 #include <Guid/SmramMemoryReserve.h>
-//#include <Ppi/DeviceRecoveryModule.h>
-//#include <Ppi/Capsule.h>
 #include <Ppi/Reset.h>
 #include <Ppi/Stall.h>
-//#include <Ppi/BootInRecoveryMode.h>
 #include <Guid/FirmwareFileSystem2.h>
 #include <Ppi/MemoryDiscovered.h>
-//#include <Ppi/RecoveryModule.h>
 #include <Ppi/Smbus2.h>
 #include <Ppi/FirmwareVolumeInfo.h>
 #include <Ppi/EndOfPeiPhase.h>
@@ -67,7 +58,6 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #include <Library/IntelQNCLib.h>
 #include <Library/PcdLib.h>
 #include <Library/SmbusLib.h>
-//#include <Library/RecoveryOemHookLib.h>
 #include <Library/TimerLib.h>
 #include <Library/PrintLib.h>
 #include <Library/ResetSystemLib.h>
