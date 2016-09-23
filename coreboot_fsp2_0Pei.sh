1e014c5778dab7c65d80f9743fadb1f196b64ca2
Lee Leahy <leroy.p.leahy@intel.com>
2016-09-23 14:58:58 -0700

    QuarkFspPkg: Support building FSP 2.0 with PEI Core
    Continue to support building FSP 2.0 with the PEI Core
    
    Change-Id: I239db1a466af7f5af614ae09aae9620058c441f0
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Lee Leahy <leroy.p.leahy@intel.com>

---
+++ coreboot_fsp2_0Pei.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+
+EDK2_DIR=$1
+COREBOOT_DIR=$2
+BUILD_TYPE=$3
+
+pushd $EDK2_DIR
+EDK2_DIR=$PWD
+popd
+
+#
+# Create the necessary directories
+#
+if [ ! -e $COREBOOT_DIR/src/soc/intel/quark/include/soc/fsp ]
+  then mkdir $COREBOOT_DIR/src/soc/intel/quark/include/soc/fsp
+fi
+
+if [ ! -e $COREBOOT_DIR/3rdparty/ ]
+then
+  mkdir $COREBOOT_DIR/3rdparty/
+fi
+if [ ! -e $COREBOOT_DIR/3rdparty/blobs/ ]
+then
+  mkdir $COREBOOT_DIR/3rdparty/blobs/
+fi
+if [ ! -e $COREBOOT_DIR/3rdparty/blobs/soc/ ]
+then
+  mkdir $COREBOOT_DIR/3rdparty/blobs/soc/
+fi
+if [ ! -e $COREBOOT_DIR/3rdparty/blobs/soc/intel/ ]
+then
+  mkdir $COREBOOT_DIR/3rdparty/blobs/soc/intel/
+fi
+if [ ! -e $COREBOOT_DIR/3rdparty/blobs/soc/intel/quark/ ]
+then
+  mkdir $COREBOOT_DIR/3rdparty/blobs/soc/intel/quark/
+fi
+
+pushd $COREBOOT_DIR/src/drivers/intel/fsp2_0/header_util/
+chmod +x fspupdvpd_sanitize.sh
+cp   $EDK2_DIR/QuarkFsp2_0PeiBinPkg/Include/FspmUpd.h   ../../../../soc/intel/quark/include/soc/fsp/FspmUpd.h
+./fspupdvpd_sanitize.sh   ../../../../soc/intel/quark/include/soc/fsp/FspmUpd.h > /dev/null
+echo FspmUpd.h - Updated
+cp   $EDK2_DIR/QuarkFsp2_0PeiBinPkg/Include/FspsUpd.h   ../../../../soc/intel/quark/include/soc/fsp/FspsUpd.h
+./fspupdvpd_sanitize.sh   ../../../../soc/intel/quark/include/soc/fsp/FspsUpd.h > /dev/null
+echo FspsUpd.h - Updated
+cp   $EDK2_DIR/QuarkFsp2_0PeiBinPkg/Include/FsptUpd.h   ../../../../soc/intel/quark/include/soc/fsp/FsptUpd.h
+./fspupdvpd_sanitize.sh   ../../../../soc/intel/quark/include/soc/fsp/FsptUpd.h > /dev/null
+echo FsptUpd.h - Updated
+cp   $EDK2_DIR/QuarkFsp2_0PeiBinPkg/Include/FspUpd.h   ../../../../soc/intel/quark/include/soc/fsp/FspUpd.h
+./fspupdvpd_sanitize.sh   ../../../../soc/intel/quark/include/soc/fsp/FspUpd.h > /dev/null
+echo FspUpd.h  - Updated
+popd
+cp $EDK2_DIR/QuarkFsp2_0PeiBinPkg/$BUILD_TYPE/*.fd $COREBOOT_DIR/3rdparty/blobs/soc/intel/quark/
